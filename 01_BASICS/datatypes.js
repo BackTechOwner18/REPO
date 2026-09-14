20656bc74b0afe413b7f6d27002e0f4e9248de7d
baljeetsingh89008 <baljeetsingh89008@gmail.com>
2026-09-14 13:03:05 +0000

    datatypes config done

---
+++ 01_BASICS/datatypes.js
@@ -0,0 +1,16 @@
+"use strict";
+let v1="string data type"
+let v2=5
+let v3='c'
+let v4=null
+
+//number -> 2 to the power of 53
+//bigint
+//string -> "   "
+//undefined -> 
+// null -> standalone value 
+// boolean ->   true/false
+
+console.log(typeof v1) //string
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
