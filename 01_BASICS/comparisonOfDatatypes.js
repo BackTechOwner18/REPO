ab98eb6ff2a9204e6711c86afa763d67dd7df560
baljeetsingh89008 <baljeetsingh89008@gmail.com>
2026-09-14 14:23:22 +0000

    comparison done

---
+++ 01_BASICS/comparisonOfDatatypes.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log("2" > 1)
+// console.log("2"== 1) // automatically string converts into number
+
+
+// console.log(null == 0); // null doesnot get converted into 0 wile using ==
+// console.log(null > 0); // null gets converted into 0 while converting - returns false in this case
+// console.log(null >= 0); // null gets converted into 0 while converting - returns true in this case
+
+
+// exact same with undefined as well
+
+ 
+console.log(undefined > true);
+console.log(undefined == true);
+console.log(undefined >=true); // when using comparison brackets < or > or <= or >= both sides gets converted into number first and are then compared
+// null -> 0 
+//undefined -> NaN
+console.log();
+
+console.log(null> true);
+console.log(null == true);
+console.log(null >=true);
+console.log(null >=false); // null -> 0 , false -> , true -> 1
+ 
