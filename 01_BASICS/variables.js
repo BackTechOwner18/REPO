183f3e01ba065e7aad23201a857ac59ed19fff4b
baljeetsingh89008 <baljeetsingh89008@gmail.com>
2026-09-14 12:47:42 +0000

    dev files config done

---
+++ 01_BASICS/variables.js
@@ -0,0 +1,12 @@
+const accountId=3232
+let accountEmail="bsbs@gmail.com"
+var accountPass="2222"
+accountCity="kanpur"
+let accountState;
+
+//accountId=3324425 // cannot be changed
+accountEmail="baljeetsingh89389@gmail.com" //can be changed
+accountPass="1123235" //can be changed
+accountCity="delhi"
+
+console.table([accountId,accountEmail, accountPass, accountCity, accountState])
