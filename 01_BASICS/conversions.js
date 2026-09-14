014c5de9ecfa25f015972c5ceb51173a9a488013
baljeetsingh89008 <baljeetsingh89008@gmail.com>
2026-09-14 13:34:30 +0000

    conversions done

---
+++ 01_BASICS/conversions.js
@@ -0,0 +1,23 @@
+let IsNumber=23
+let StringIsNum=String(IsNumber)
+//console.log(StringIsNum);
+//console.log(typeof StringIsNum);
+
+let IsString="424"
+let NumberIsString=Number(IsString)
+//+console.log(NumberIsString)
+//console.log(typeof NumberIsString)
+ let isBoolean =false
+ let NumberIsBoolean=Number(isBoolean)
+ //console.log(NumberIsBoolean)
+ //console.log(typeof NumberIsBoolean)
+
+  isBoolean =false
+ let StringIsBoolean=String(isBoolean)
+ //nsole.log(StringIsBoolean)
+//onsole.log(typeof StringIsBoolean)
+
+ IsNumber=4242
+ let BooleanIsNum=Boolean(IsNumber)
+ console.log(BooleanIsNum)
+ console.log(typeof BooleanIsNum)
