ef36644fdb4dda9155ec811717bfcd1ada28c230
Baljeet Singh Bhatia <baljeetsingh89008@gmail.com>
2026-09-15 07:28:11 +0000

    numbers and maths fnctions

---
+++ 01_BASICS/numbersAndMaths.js
@@ -0,0 +1,45 @@
+    //    NUMBER FUNCTIONS 
+
+
+// const score = 400
+// console.log(score.toString());
+// console.log(typeof score.toString());
+
+// const balance=10000000.462965234
+// console.log(balance.toPrecision(13));
+
+// console.log(balance.toLocaleString());
+// console.log(balance.toLocaleString('en-IN'));
+
+
+
+
+// const test = 10.45745
+// console.log(test.toPrecision(5));
+// console.log(test.toLocaleString());
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+                            // MATHS VALUES 
+
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6546))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(5.9))
+
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10+1))
+
+const max = 25
+const min = 15
+
+console.log(Math.floor(Math.random()*(max-min+1)+ min)) //prints random numbers bw 15 and 25
+
+
+
