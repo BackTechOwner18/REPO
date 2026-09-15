a08b26903cf80450106e933f8a5408bfa7cb8f1a
Baljeet Singh Bhatia <baljeetsingh89008@gmail.com>
2026-09-15 06:45:40 +0000

    stringprototypes and functions

---
+++ 01_BASICS/stringfunctions.js
@@ -0,0 +1,41 @@
+let NewString= new String('baljeet-bs')
+// console.log(NewString.length)
+// console.log(NewString.substring(1,5))
+// console.log(NewString.substring(-6,1));
+
+// console.log(NewString.slice(-7,7))
+
+// console.log(NewString.toUpperCase())
+
+// console.log(NewS
+let indexOfdash = NewString.indexOf('-')
+// console.table([NewString.substring(0,indexOfdash), NewString.substring(indexOfdash+1, NewString.length)])
+
+let url = "google.com//who%20is%20baljeet%20singh"
+// console.table([url, url.replace('%20', ' ')])
+// // console.table([url, url.replaceAll('%20', ' ')])
+
+
+// let MyName2= "baljeet"
+// let className= '12th'
+// console.log(`hello world , my name is ${MyName2} and i study in class ${className}`)
+
+// console.log(url.includes('@gmail.com'))
+
+let NewS2= "                baljeet                 "
+
+// console.table([NewS2, NewS2.trim(), NewS2.replaceAll(' ','')])
+
+
+// split function 
+
+let news3= '  my   name   is   baljeet singh and i study in class 12th  '
+
+// console.log(news3.split(' '))
+// console.log(news3.split(' ').length)
+// console.table([news3.trim(), news3])
+
+
+// console.log(news3.split(' ',13))
+
+
