ddfe1832596156aace3de99d80a8ac30a3578ff0
Baljeet Singh Bhatia <baljeetsingh89008@gmail.com>
2026-09-16 17:32:12 +0000

    objects part 1

---
+++ 02_BASICS/objects.js
@@ -0,0 +1,45 @@
+//singleton
+//object created using constructor method
+
+//object declaration without singleton
+
+const NewSym = Symbol("baljeet singh")
+const myObj = {
+    name : 'baljeet',
+    age : 17 ,
+    location : 'kanpur',
+    IsLoggedIn : false,
+    "full name" : "baljeet singh bhatia",
+    [NewSym] : "neWKEY1"
+}
+
+// accessing objects components
+// console.log(myObj.name)
+// console.log(typeof myObj.name)
+
+// console.log(myObj['full name'])
+// console.log(myObj['full name'])
+
+// console.log(myObj[NewSym])
+
+// console.log(typeof myObj[NewSym])
+
+// Object.freeze(myObj)
+// myObj.name = "nsnjbea"
+// console.log(myObj);
+
+// using functions inside objects 
+
+myObj.greeting = function() {
+    console.log("Hello JS User");
+    
+}
+
+    myObj.greetingTwo = function() {
+    console.log(`Hello JS User , ${this.name}`);
+    
+}
+
+console.log(myObj.greeting())
+console.log(myObj.greetingTwo())
+
